d81ee07e5643a02bb3d0bbc315c40b73db15fa31
RedWilly <thompsonau023@gmail.com>
2025-06-04 21:32:04 +0000

    better-auth Intergration ...

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+import {
+    betterAuth
+} from 'better-auth';
+import { PrismaClient } from "@prisma/client";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+const db = new PrismaClient();
+
+export const auth = betterAuth({
+    database: prismaAdapter(db, {
+		provider: "postgresql",
+	}),
+    emailAndPassword: {
+        enabled: true,
+        async sendResetPassword(data, request) {
+            // Send an email to the user with a link to reset their password
+        },
+    },
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!
+        }
+    },
+
+    /** if no database is provided, the user data will be stored in memory.
+     * Make sure to provide a database to persist user data **/
+});
